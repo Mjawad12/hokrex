f305589a7e2fe8588bb5eeacd156708f776b1405
Mjawad12 <143606319+Mjawad12@users.noreply.github.com>
2024-04-19 20:02:47 +0500

    homepage

--- src/app/(store)/page.jsx
+++ src/app/(store)/page.jsx
@@ -1,4 +1,4 @@
-import { left2, star } from "@/Consonats";
+import { left2, prods, star } from "@/Consonats";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -7,7 +7,7 @@ export default async function page() {
   const images = [0, 1, 2, 3];
   return (
     <div className="flex max-w-[1450px] m-auto px-5 min-h-[calc(100vh-65px)] ">
-      <div className="flex-1 flex-grow-[0.5] flex justify-center flex-col gap-[7rem]">
+      <div className="flex-1 flex-grow-[0.43] flex justify-center flex-col gap-[7rem]">
         <div className="flex flex-col gap-3">
           <p className="text-[22px] font-[700] tracking-[5px] ">UNLEASH YOUR</p>
           <h1 className="text-[75px] font-[700] leading-[55px]">
@@ -64,7 +64,56 @@ export default async function page() {
           </div>
         </div>
       </div>
-      <div className="flex-1 flex-grow-[0.5]"></div>
+      <div className="flex-1 flex-grow-[0.6] flex-center ">
+        <div className="flex-center flex-col relative max-w-[48rem] w-full">
+          <div className="absolute top-[-16px] w-full right-[5.9rem] flex justify-end ">
+            <Image
+              src={"/homepage/label2.webp"}
+              width={150}
+              height={150}
+              alt="label"
+            />
+          </div>
+          <div className="absolute bottom-0 w-full left-[3rem] z-10">
+            <Image
+              src={"/homepage/label1.webp"}
+              width={180}
+              height={180}
+              alt="label"
+            />
+          </div>
+          <div className="absolute top-[4.2rem] z-20 translate-x-3">
+            <Image
+              src={"/homepage/text.webp"}
+              alt="text edit"
+              width={290}
+              height={290}
+            />
+          </div>
+          <div className=" bg-[#FFDBDB] w-[25rem] h-[25rem] rounded-full absolute bottom-[5px] left-[50%] translate-x-[-52%]"></div>
+          <div className="flex flex-col gap-5 [&_svg]:stroke-black absolute bottom-2 right-0 w-[9.8rem] ">
+            <span className="w-[3.5rem] h-[3.5rem] rounded-full flex-center bg-[#CFE2FF] [&_svg]:relative [&_svg]:right-[1px] relative right-[0.8rem]  ">
+              {prods[3].svg}
+            </span>
+            <span className="w-[3.5rem] h-[3.5rem] rounded-full flex-center bg-[#CFFFF9] [&_svg]:relative relative right-1 ">
+              {prods[2].svg}
+            </span>
+            <span className="[&_svg]:fill-black [&_svg]:stroke-none w-[3.5rem] h-[3.5rem] rounded-full flex-center bg-[#FECFFF] [&_svg]:relative [&_svg]:top-[2px] [&_svg]:right-[1.5px] relative right-4">
+              {prods[5].svg}
+            </span>
+            <span className="w-[3.5rem] h-[3.5rem] rounded-full flex-center bg-[#FFE9CF] [&_svg]:relative [&_svg]:top-[1px] relative right-[4rem] bottom-2">
+              {prods[6].svg}
+            </span>
+          </div>
+          <Image
+            src={"/homepage/shirtFront.webp"}
+            alt="shirt"
+            width={450}
+            height={450}
+            className="z-10"
+          />
+        </div>
+      </div>
     </div>
   );
 }
