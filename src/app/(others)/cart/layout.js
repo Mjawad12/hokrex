4330b65f4a7a2716fb1c45bf274b453307a0a0d4
Mjawad12 <143606319+Mjawad12@users.noreply.github.com>
2024-06-29 23:15:36 +0500

    updtae

--- src/app/(others)/cart/layout.js
+++ src/app/(others)/cart/layout.js
@@ -1,6 +1,7 @@
 import Mainstatecart from "@/components/Mainstate(cart)/Mainstatecart";
 import { Inter } from "next/font/google";
 import "../../globals.css";
+import Mainstatestore from "@/components/Mainstate(store)/Mainstatestore";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
@@ -13,7 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Mainstatecart>{children}</Mainstatecart>
+        <Mainstatestore>
+          <Mainstatecart>{children}</Mainstatecart>
+        </Mainstatestore>
       </body>
     </html>
   );
