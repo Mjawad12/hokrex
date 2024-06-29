1b56cb48db610dbef00b59ebbb801936d3baf3c0
Mjawad12 <143606319+Mjawad12@users.noreply.github.com>
2024-06-29 22:20:33 +0500

    update

--- src/app/api/getOrders/route.js
+++ src/app/api/getOrders/route.js
@@ -8,34 +8,34 @@ export async function POST() {
   try {
     await ConnectDb();
     const headersList = headers();
-    if (headersList.get("origin") === process.env.NEXT_PUBLIC_URL) {
-      const id = await authorize(headersList.get("authToken"));
-      if (!id) {
-        return Response.json({ success: false, msg: "Not Authorized" });
-      }
-      const Admin = await UserSchema.findOne({ _id: id });
-      if (Admin.name === "jamsheed-9-123") {
-        let users = await UserSchema.find({});
-        let orders = [];
-        for (let i = 0; i < users.length; i++) {
-          for (let k = 0; k < users[i].orders.length; k++) {
-            let product = await ProductSchema.findOne({
-              _id: users[i].orders[k].product,
-            });
-            users[i].orders[k].product = product;
-          }
-          if (users[i].orders.length > 0) {
-            orders = [...orders, ...users[i].orders];
-          }
-        }
-
-        return Response.json({ success: true, orders: orders });
-      }
+    // if (headersList.get("origin") === process.env.NEXT_PUBLIC_URL) {
+    const id = await authorize(headersList.get("authToken"));
+    if (!id) {
+      return Response.json({ success: false, msg: "Not Authorized1" });
     }
-    return Response.json(
-      { success: false, err: "Not Authorized" },
-      { status: 500 },
-    );
+    const Admin = await UserSchema.findOne({ _id: id });
+    if (Admin.name === "jamsheed-9-123") {
+      let users = await UserSchema.find({});
+      let orders = [];
+      for (let i = 0; i < users.length; i++) {
+        for (let k = 0; k < users[i].orders.length; k++) {
+          let product = await ProductSchema.findOne({
+            _id: users[i].orders[k].product,
+          });
+          users[i].orders[k].product = product;
+        }
+        if (users[i].orders.length > 0) {
+          orders = [...orders, ...users[i].orders];
+        }
+      }
+
+      return Response.json({ success: true, orders: orders });
+    }
+    // }
+    // return Response.json(
+    //   { success: false, err: "Not Authorized2" },
+    //   { status: 500 },
+    // );
   } catch (error) {
     console.log(error.message);
     return Response.json(
